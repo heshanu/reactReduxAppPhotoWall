29e006055b31acc649d0cdc537ed291430ac439b
heshanu <heshanu97@gmail.com>
2022-06-18 22:59:10 +0530

    add some ui designs

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
 class Photo extends Component {
   render() {
-    // eslint-disable-next-line no-unreachable
-    //const post=this.props.post;
-    return <figure className="figure"></figure> 
+    const post = this.props.post;
+    return (
+      <figure className="figure">
+        <img className="photo" src={post.imageLink} alt={post.description} />
+        <figcaption>
+          <p className="p">{post.description}</p>
+        </figcaption>
+        <div className="button-container">
+          <button className="remove-button">Remove</button>
+        </div>
+      </figure>
+    );
   }
 }
 
