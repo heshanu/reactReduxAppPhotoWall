0f978cf0f43acedf6ff12ec27903078f67b60bb6
heshanu <heshanu97@gmail.com>
2022-06-18 23:12:58 +0530

    add function instead of class and change it this key word

--- src/Components/Photowall.js
+++ src/Components/Photowall.js
@@ -1,16 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 import Photo from "./Photo";
-import '../styles/stylesheet.css';
-class Photowall extends Component {
-  render() {
-    return (
-      <div className="photoGrid">
-        {this.props.posts.map((post, index) =>
-          <Photo key={index} post={post} />
-        )}
-      </div>
-    )
-  }
+import "../styles/stylesheet.css";
+
+function Photowall(props) {
+  return (
+    <div className="photoGrid">
+      {props.posts.map((post, index) => (
+        <Photo key={index} post={post} />
+      ))}
+    </div>
+  );
 }
 
 export default Photowall;
