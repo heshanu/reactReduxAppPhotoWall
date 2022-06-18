1c9dee1e68cf4222284060b375db535d974b2e7a
heshanu <heshanu97@gmail.com>
2022-06-18 22:28:06 +0530

    added

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 class Photo extends Component {
   render() {
     // eslint-disable-next-line no-unreachable
+    //const post=this.props.post;
     return <figure className="figure"></figure> 
   }
 }
